02cde754e84a7946bcadd22675b474a24b56427a
Nhat Linh Nguyen <ronoved3v@gmail.com>
2023-09-23 13:05:21 +0700

    Update

--- server/src/api/v1/routes/testFlight/index.js
+++ server/src/api/v1/routes/testFlight/index.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
 const testFlightController = require("../../controllers/testflight/index");
 
+// Get All TestFlight
+router.get("/", testFlightController.testFlight);
+
 // Add TestFlight
 router.post("/add", testFlightController.testFlightAdd);
-router.get("/", testFlightController.testFlight);
+
+// Delete TestFlight
+router.delete("/delete", (req, res) => {
+  res.send(200);
+});
 
 module.exports = router;
